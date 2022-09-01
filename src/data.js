e6c01f3fd9dd9dca1e84646df055081f3713beb4
Starniraj <singhniraj637@gmail.com>
2022-09-01 22:56:28 +0530

    first

---
+++ src/data.js
@@ -0,0 +1,78 @@
+const data = {
+   
+        products: [
+          {
+            id: '1',
+            name: 'MacBook',
+            price: 1400,
+            image: 'https://picsum.photos/id/180/2400/1600',
+          },
+          {
+            id: '2',
+            name: 'Old Car',
+            price: 2400,
+            image: 'https://picsum.photos/id/111/4400/2656',
+          },
+          {
+            id: '3',
+            name: 'Iphone 11',
+            price: 600,
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWvIzZP3RD-bCxw90VQNgh07sXnnEoze1bbw&usqp=CAU',
+          },
+      
+      {
+        id: '4',
+        name: 'Headphone',
+        price: 200,
+        image: 'https://images-eu.ssl-images-amazon.com/images/I/31x-Xz8TkbL._SX300_SY300_QL70_FMwebp_.jpg',
+      },
+      {
+        id: '5',
+        name: 'Iphone 11',
+        price: 700,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWvIzZP3RD-bCxw90VQNgh07sXnnEoze1bbw&usqp=CAU',
+      },
+      // {
+      //   id: '6',
+      //   name: 'MacBook',
+      //   price: 1400,
+      //   image: 'https://picsum.photos/id/180/2400/1600',
+      // },{
+      //   id: '7',
+      //   name: 'MacBook',
+      //   price: 1400,
+      //   image: 'https://picsum.photos/id/180/2400/1600',
+      // },
+      // {
+      //   id: '8',
+      //   name: 'MacBook',
+      //   price: 1400,
+      //   image: 'https://picsum.photos/id/180/2400/1600',
+      // },
+      // {
+      //   id: '9',
+      //   name: 'MacBook',
+      //   price: 1400,
+      //   image: 'https://picsum.photos/id/180/2400/1600',
+      // },
+      // {
+      //   id: '10',
+      //   name: 'MacBook',
+      //   price: 1400,
+      //   image: 'https://picsum.photos/id/180/2400/1600',
+      // },
+      // {
+      //   id: '11',
+      //   name: 'MacBook',
+      //   price: 1400,
+      //   image: 'https://picsum.photos/id/180/2400/1600',
+      // },
+      // {
+      //   id: '12',
+      //   name: 'MacBook',
+      //   price: 1400,
+      //   image: 'https://picsum.photos/id/180/2400/1600',
+      // },
+    ],
+  };
+  export default data;
